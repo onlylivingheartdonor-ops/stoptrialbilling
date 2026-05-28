9eea4f657aaf925f8b6e81a7f08835a750485554
David M. Graham <onlylivingheartdonor.com>
2026-05-28 11:31:15 -0400

    Upgrade to Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'stoptrialbilling.com' }],
+        destination: 'https://www.stoptrialbilling.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
